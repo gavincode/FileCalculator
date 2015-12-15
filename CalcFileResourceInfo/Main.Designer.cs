ccc9fa3bcdfb052d9301a984aa6dfd333bba9821
gavin <709181180@qq.com>
2015-12-15 15:26:15 +0800

    init project

---
+++ CalcFileResourceInfo/Main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CalcFileResourceInfo
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChooseFile = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblMD5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请选择文件: ";
+            // 
+            // txtChooseFile
+            // 
+            this.txtChooseFile.AllowDrop = true;
+            this.txtChooseFile.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtChooseFile.ForeColor = System.Drawing.Color.Black;
+            this.txtChooseFile.Location = new System.Drawing.Point(100, 22);
+            this.txtChooseFile.Name = "txtChooseFile";
+            this.txtChooseFile.Size = new System.Drawing.Size(388, 21);
+            this.txtChooseFile.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtChooseFile, "*支持拖拽和粘贴文件");
+            this.txtChooseFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.txtChooseFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            this.txtChooseFile.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtChooseFile_KeyDown);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(506, 19);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(65, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(27, 32);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(41, 12);
+            this.lblSize.TabIndex = 3;
+            this.lblSize.Text = "大小: ";
+            // 
+            // lblMD5
+            // 
+            this.lblMD5.AutoSize = true;
+            this.lblMD5.Location = new System.Drawing.Point(29, 66);
+            this.lblMD5.Name = "lblMD5";
+            this.lblMD5.Size = new System.Drawing.Size(35, 12);
+            this.lblMD5.TabIndex = 4;
+            this.lblMD5.Text = "MD5: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtMD5);
+            this.groupBox1.Controls.Add(this.txtSize);
+            this.groupBox1.Controls.Add(this.lblSize);
+            this.groupBox1.Controls.Add(this.lblMD5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "信息";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(487, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "byte";
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(88, 61);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.ReadOnly = true;
+            this.txtMD5.Size = new System.Drawing.Size(388, 21);
+            this.txtMD5.TabIndex = 6;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(88, 28);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.ReadOnly = true;
+            this.txtSize.Size = new System.Drawing.Size(388, 21);
+            this.txtSize.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(590, 166);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtChooseFile);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文件信息计算工具";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Main_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChooseFile;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblMD5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ CalcFileResourceInfo/Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcFileResourceInfo
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            String filePath = txtChooseFile.Text;
+
+            CalcFileInfo(filePath);
+        }
+
+        private void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            var items = e.Data.GetData(DataFormats.FileDrop) as String[];
+
+            if (items != null && items.Length > 0)
+            {
+                txtChooseFile.Text = items[0];
+
+                CalcFileInfo(items[0]);
+            }
+        }
+
+        private void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void CalcFileInfo(String filePath)
+        {
+            if (String.IsNullOrWhiteSpace(filePath)) return;
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show(String.Format("文件绝对路径:\"{0}\"不存在!", filePath));
+                return;
+            }
+
+            try
+            {
+                var bytes = File.ReadAllBytes(filePath);
+
+                txtSize.Text = bytes.Length.ToString();
+                txtMD5.Text = Moqikaka.Util.Security.MD5Util.MD5(bytes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("文件计算失败, 错误信息: {0}", ex.Message + Environment.NewLine + ex.StackTrace));
+            }
+        }
+
+        private void txtChooseFile_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.V || e.Modifiers != Keys.Control) return;
+
+            IDataObject iData = Clipboard.GetDataObject();
+
+            if (iData.GetDataPresent(DataFormats.FileDrop))
+            {
+                var items = iData.GetData(DataFormats.FileDrop) as String[];
+
+                if (items != null && items.Length > 0)
+                {
+                    txtChooseFile.Text = items[0];
+
+                    CalcFileInfo(items[0]);
+                }
+            }
+        }
+    }
+}
